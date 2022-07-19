b1e5893a2b73db4ca7898ad1c1b65b5f9ccd4551
apip11 <fatchullahwahid@gmail.com>
2022-07-19 16:58:19 +0700

    tambah controller list dan save

--- src/main/java/com/example/coffe/controller/MenuController.java
+++ src/main/java/com/example/coffe/controller/MenuController.java
@@ -7,6 +7,8 @@ import com.example.coffe.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,32 +19,96 @@ public class MenuController {
     private MenuRepository menuRepository;
 
     @PostMapping("/pilihan")
-    public DefaultResponse pilihan(@RequestBody MenuDto menuDto){
+    public DefaultResponse pilihan(@RequestBody MenuDto menuDto) {
 
         Optional<Menu> optionalMenu = menuRepository.findById(menuDto.getIdMenu());
         DefaultResponse df = new DefaultResponse();
-        if (optionalMenu.isPresent()){
+        if (optionalMenu.isPresent()) {
             df.setStatus(Boolean.TRUE);
-            df.setMessage("Menu yang anda pilih telah tersimpan ");
-        } else{
+            df.setMessage("MENU YANG ANDA PILIH TELAH TERSIMPAN");
+        } else {
             df.setStatus(Boolean.FALSE);
-            df.setMessage("Menu yang anda pilih tidak tersedia");
+            df.setMessage("MENU YANG ANDA PILIH TIDAK TERSEDIA");
         }
         return df;
     }
+
     @GetMapping("/byid/{idmenu}")
-    public DefaultResponse getByidMenu(@PathVariable Integer idMenu){
+    public DefaultResponse getByidMenu(@PathVariable Integer idMenu) {
 
         DefaultResponse df = new DefaultResponse();
-        Optional<Menu> menuOps =  menuRepository.findById(idMenu);
-        if (menuOps.isPresent()){
+        Optional<Menu> menuOps = menuRepository.findById(idMenu);
+        if (menuOps.isPresent()) {
             df.setStatus(Boolean.TRUE);
-            df.setMessage("Menu yang anda pilih telah tersimpan );
-        } else{
+            df.setMessage("MENU YANG ANDA PILIH TELAH TERSIMPAN");
+        } else {
             df.setStatus(Boolean.FALSE);
-            df.setMessage("Menu yang anda pilih tidak tersedia");
+            df.setMessage("MENU YANG ANDA PILIH TIDAK TERSEDIA");
         }
 
         return df;
+    }
+
+    @PostMapping("/save")
+    public DefaultResponse<MenuDto> saveMenu(@RequestBody MenuDto menuDto) {
+        Menu menu = convertDtoToEntity(menuDto);
+        DefaultResponse<MenuDto> response = new DefaultResponse<>();
+        Optional<Menu> optional = menurepository.findByidMenu(menuDto.getIdMenu());
+        if (optional.isPresent()) {
+            response.setMessage("ERROR, DATA MENU TELAH TERSEDIA");
+        } else {
+            menurepository.save(menu);
+            response.setMessage("DATA MENU BERHASIL TERSIMPAN");
+            response.setData(menuDto);
+        }
+        return response;
+    }
+
+    @GetMapping("/listmenu")
+    public List<MenuDto> getListMenu() {
+        List<MenuDto> list = new ArrayList<>();
+        for (Menu m : menurepository.findAll()) {
+            list.add(convertEntityToDto(m));
+        }
+        return list;
+    }
+
+    public List<MenuDto> listData() {
+        List<MenuDto> list = new ArrayList<>();
+        MenuDto m = new MenuDto();
+        m.setIdMenu(001);
+        m.setIdjenis(001);
+        m.setNamaMenu("Kopi Busuk");
+        m.setStock(50);
+        m.setHarga(10000.0);
+
+        list.add(m);
+
+        MenuDto menuDto = new MenuDto();
+        menuDto.setIdMenu(002);
+        menuDto.setIdjenis(001);
+        menuDto.setNamaMenu("Kopi Susu");
+        menuDto.setStock(50);
+        menuDto.setHarga(8000.0);
+        list.add(menuDto);
+        return list;
+    }
+
+    public Menu convertDtoToEntity(MenuDto dto) {
+        Menu entity = new Menu();
+        entity.setIdMenu(dto.getIdMenu());
+        entity.setNamaMenu(dto.getNamaMenu());
+        entity.setStock(dto.getStock());
+        entity.setHarga(dto.getHarga());
+        return entity;
+    }
+
+    public MenuDto convertEntityToDto(Menu entity) {
+        MenuDto dto = new MenuDto();
+        dto.setIdMenu(dto.getIdMenu());
+        dto.setNamaMenu(dto.getNamaMenu());
+        dto.setStock(dto.getStock());
+        dto.setHarga(dto.getHarga());
+        return dto;
     }
 }
